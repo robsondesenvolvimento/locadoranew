bb3252db3900d587a0c027ab5812bcb8193ae330
Robson Alves <robsondesenvolvimento@gmail.com>
2021-09-23 00:01:20 -0300

    fix: modularização do código de geração de log e callbacks.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-// Chama uma promisse e retorna log com data no formato ISO e opcionalmente executa uma tarefa.
-function awaitMessageAsync(log, fn){
-    var promisse = async () => await new Promise(resolve => resolve(new Date().toISOString()));
-    promisse().then(date => {
-        console.log(`${date} ${log}`)
-        if (fn !== undefined) fn();
-    });
-}
+const awaitLogAsync = require('./common/awaitLogAsync')
 
-awaitMessageAsync(`Iniciando execução da api locadora...`, () => console.log("Tarefa executada"));
-awaitMessageAsync(`Terminando execução da api locadora...`);
+awaitLogAsync(`Iniciando execução da api locadora...`, () => console.log("Tarefa executada"));
+awaitLogAsync(`Terminando execução da api locadora...`);
